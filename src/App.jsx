6313da6899722432585bf13cf52e79dd4f7277d9
MohdMusadiq <mdmusadiq177@gmail.com>
2026-09-11 13:04:27 +0530

    Uploaded complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
+
 import Navbar from "./components/Navbar";
-import Welcome from "./components/Welcome";
 import Dashboard from "./components/Dashboard";
+import Tasks from "./components/Tasks";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div> 
+        <div>
             <Navbar />
-            <Welcome />
-            <Dashboard />
+
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/tasks" element={<Tasks />} />
+            </Routes>
         </div>
     );
 }
+
 export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,28 +1,99 @@
-import StackCard from "./StackCard";
+import StatCard from "./StatCard";
 import TaskCard from "./TaskCard";
+import AddTask from "./AddTask";
+import { useState } from "react";
+
 function Dashboard() {
-    const tasks = [{title: "Learn React", description: "Understanding Components", status: "In Progress"}, 
-        {title: "learn JavaScript", description: "Understanding Variables and Functions", status: "Not Started"},
-         {title: "learn Python", description: "Understanding Basics", status: "In Progress"}];
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: "Learn React",
+            description: "Understanding components",
+            status: "Completed"
+        },
+        {
+            id: 2,
+            title: "Learn JavaScript",
+            description: "Understanding variables and functions",
+            status: "Pending"
+        },
+        {
+            id: 3,
+            title: "Learn Python",
+            description: "Understanding commands",
+            status: "Completed"
+        }
+    ]);
+
+    function toggleTask(id) {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) => {
+                if (task.id === id) {
+                    return {
+                        ...task,
+                        status:
+                            task.status === "Completed"
+                                ? "Pending"
+                                : "Completed"
+                    };
+                }
+
+                return task;
+            })
+        );
+    }
+
+    function addTask(newTask) {
+        setTasks((prevTasks) => [
+            ...prevTasks,
+            {
+                ...newTask,
+                id: Date.now()
+            }
+        ]);
+    }
+    function deleteTask(id) {
+    setTasks((prevTasks) =>
+        prevTasks.filter((task) => task.id !== id)
+    );
+}
+
+
+    const totalTasks = tasks.length;
+    const completedTasks = tasks.filter(
+        (task) => task.status === "Completed"
+    ).length;
+    const pendingTasks = tasks.filter(
+        (task) => task.status === "Pending"
+    ).length;
+
     return (
         <main>
             <div className="stats-container">
-                <StackCard title="Total Tasks" value="10" />
-                <StackCard title="completed" value="6" />
-                <StackCard title="Pending" value="4" />
-                <StackCard title="time remaining" value="2h 30m" />
-                <StackCard title=" Name" value="Unknown" />
-
+                <StatCard title="Total Tasks" value={totalTasks} />
+                <StatCard title="Completed" value={completedTasks} />
+                <StatCard title="Pending" value={pendingTasks} />
             </div>
+
+            <AddTask onAddTask={addTask} />
+
             <h2>Recent Tasks</h2>
-            <div className="task-container">
-                <TaskCard  title="Learn React" description="Understanding Components" status="In Progress" />
-                <TaskCard title="learn JavaScript" description="Understanding Variables and Functions" status="Not Started" />
-                <TaskCard title="learn Python" description="Understanding Basics" status="In Progress" />
+
+            <div className="tasks-container">
+                {tasks.map((task) => (
+                    <TaskCard
+                        key={task.id}
+                        title={task.title}
+                        description={task.description}
+                        status={task.status}
+                        onToggle={() => toggleTask(task.id)}
+                        onDelete={() => deleteTask(task.id)}
+                    />
+                ))}
             </div>
-
-
         </main>
     );
 }
+
+
 export default Dashboard;
